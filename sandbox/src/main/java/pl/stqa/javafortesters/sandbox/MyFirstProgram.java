bd3673ed4dc97d64139ba38e5d3f861d4fc59f10
Natalia Ziajka <nataliaziajka@gmail.com>
2019-12-18 22:41:57 +0100

    Dodano kilka funkcji

--- sandbox/src/main/java/pl/stqa/javafortesters/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/pl/stqa/javafortesters/sandbox/MyFirstProgram.java
@@ -2,7 +2,29 @@ package pl.stqa.javafortesters.sandbox;
 
 public class MyFirstProgram {
 
- public static void main(String[] args){
-  System.out.println("Hello, world!");
-}
+ public static void main(String[] args) {
+
+  hello("world");
+  hello("user");
+  hello("Alexei");
+
+  double l = 5;
+  System.out.println("Powierzchnia kwadratu o boku" + l + "=" + area(l));
+
+  double a = 4;
+  double b = 6;
+
+  System.out.println("Powierzchnia prostokata o boku"  + a + " i boku" + b + "=" + area(a, b));
+ }
+
+  public static  void hello(String somebody){
+   System.out.println("Hello" + somebody + "!");
+ }
+   public static double area(double len){
+   return len *len;
+   }
+   public static double area(double a, double b){
+    return a*b;
+   }
+
 }
