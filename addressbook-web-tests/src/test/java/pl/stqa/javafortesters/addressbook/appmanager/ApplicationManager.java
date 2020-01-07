4c5353ab6af965a22466375d2af8187eb55d99fd
Natalia Ziajka <nataliaziajka@gmail.com>
2020-01-07 16:57:32 +0100

    Refactor code to run tests on different browsers

--- addressbook-web-tests/src/test/java/pl/stqa/javafortesters/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/pl/stqa/javafortesters/addressbook/appmanager/ApplicationManager.java
@@ -1,8 +1,12 @@
 package pl.stqa.javafortesters.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
@@ -12,9 +16,23 @@ public class ApplicationManager {
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void init() {
-    wd = new FirefoxDriver();
+    String browser = BrowserType.FIREFOX;
+    if(Objects.equals(browser, BrowserType.FIREFOX)){
+      wd = new FirefoxDriver();
+    }
+    else if(Objects.equals(browser, BrowserType.CHROME)){
+      wd = new ChromeDriver();
+    }
+    else if(Objects.equals(browser, BrowserType.IE)){
+      wd = new InternetExplorerDriver();
+    }
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
     contactHelper = new ContactHelper(wd);
